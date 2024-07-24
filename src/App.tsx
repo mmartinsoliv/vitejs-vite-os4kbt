25848888c88cee80e9c7157f612ec133b528c285
mmartinsoliv <mathews_2010@outlook.com>
2024-07-24 17:11:38 -0300

    again

--- src/App.tsx
+++ src/App.tsx
@@ -5,60 +5,44 @@ import './App.css'
 
 function App() {
   const [count, setCount] = useState(0)
-  const [isScriptLoaded, setIsScriptLoaded] = useState(false);
+  const [isScriptCalled, setIsScriptCalled] = useState(false);
   const [isLoading, setIsLoading] = useState(false);
   const [loadError, setLoadError] = useState(false);
 
-  useEffect(() => {
-    // Adiciona o script ao head do documento quando o componente é montado
-    const script = document.createElement('script');
-    script.id = 'maze-script';
-    script.src = 'https://snippet.maze.co/maze-universal-loader.js?apiKey=4251a303-735e-4187-b751-5550fe15943c';
-    script.async = true;
-    script.onload = () => {
-      setIsScriptLoaded(true);
-      console.log('Maze script loaded successfully.');
-    };
-    script.onerror = () => {
-      setLoadError(true);
-      console.error('Error loading maze script.');
-    };
-    document.head.appendChild(script);
-
-    // Limpeza ao desmontar o componente
-    return () => {
-      document.head.removeChild(script);
-    };
-  }, []);
-
   const activateMazeScript = () => {
-    if (!isScriptLoaded || isLoading) return; // Verifica se o script está carregado e não está carregando novamente
+    if (isScriptCalled || isLoading) return; // Evita chamar o script novamente se já estiver chamado
 
     setIsLoading(true);
     setLoadError(false);
 
     // Executa o código JavaScript fornecido
-    (function (m, a, z, e) {
-      var s, t;
-      try {
-        t = m.sessionStorage.getItem('maze-us');
-      } catch (err) {}
-
-      if (!t) {
-        t = new Date().getTime();
+    try {
+      (function (m, a, z, e) {
+        var s, t;
         try {
-          m.sessionStorage.setItem('maze-us', t);
+          t = m.sessionStorage.getItem('maze-us');
         } catch (err) {}
-      }
 
-      s = a.createElement('script');
-      s.src = z + '?apiKey=' + e;
-      s.async = true;
-      a.getElementsByTagName('head')[0].appendChild(s);
-      m.mazeUniversalSnippetApiKey = e;
+        if (!t) {
+          t = new Date().getTime();
+          try {
+            m.sessionStorage.setItem('maze-us', t);
+          } catch (err) {}
+        }
 
+        s = a.createElement('script');
+        s.src = z + '?apiKey=' + e;
+        s.async = true;
+        a.getElementsByTagName('head')[0].appendChild(s);
+        m.mazeUniversalSnippetApiKey = e;
+      })(window, document, 'https://snippet.maze.co/maze-universal-loader.js', '4251a303-735e-4187-b751-5550fe15943c');
+      
+      setIsScriptCalled(true); // Marca o script como chamado
       setIsLoading(false);
-    })(window, document, 'https://snippet.maze.co/maze-universal-loader.js', '4251a303-735e-4187-b751-5550fe15943c');
+    } catch (error) {
+      setLoadError(true);
+      console.error('Error executing maze script:', error);
+    }
   };
 
 
