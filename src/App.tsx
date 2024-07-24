9cf35b1d1e6e10d2ed86ad74c62692114a3ab0c6
mmartinsoliv <mathews_2010@outlook.com>
2024-07-24 17:43:38 -0300

    update

--- src/App.tsx
+++ src/App.tsx
@@ -1,41 +1,9 @@
-import { useEffect } from 'react'
 import './App.css'
 
 function App() {
-
-  useEffect(() => {
-    (function (m, a, z, e) {
-      var s, t;
-      try {
-        t = m.sessionStorage.getItem('maze-us');
-      } catch (err) {}
-      if (!t) {
-        t = new Date().getTime();
-        try {
-          m.sessionStorage.setItem('maze-us', t as any);
-        } catch (err) {}
-      }
-      s = a.createElement('script');
-      s.src = z + '?apiKey=' + e;
-      s.async = true;
-      a.getElementsByTagName('head')[0].appendChild(s);
-      m.mazeUniversalSnippetApiKey = e;
-    })(window, document, 'https://snippet.maze.co/maze-universal-loader.js', 'c572e4d3-85d5-479e-b98c-69c97862bfa1');
-  }, []);
-  const handleMazeButtonClick = () => {
-    if (window.mazeUniversalSnippetApiKey) {
-      if (window.Maze) {
-        window.Maze.activatePrompt();
-      } else {
-        console.error('Maze script not loaded yet.');
-      }
-    } else {
-      console.error('Maze API key not found. Ensure the Maze snippet is correctly integrated.');
-    }
-  };
   return (
     <div className="App">
-      <button onClick={handleMazeButtonClick}>Ativar Maze</button>
+      <button>Ativar Mazeee</button>
     </div>
   );
 }
