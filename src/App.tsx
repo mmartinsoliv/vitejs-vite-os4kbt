1e4e264ebb1d5a56bf7f5aa748acb2b0d00f7eb2
mmartinsoliv <mathews_2010@outlook.com>
2024-07-24 17:49:02 -0300

    updagre

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,9 @@
-import { useEffect } from 'react'
+import { useEffect, useState } from 'react'
 import './App.css'
 
 function App() {
+  const [isMazeLoaded, setIsMazeLoaded] = useState(false);
+
   useEffect(() => {
     (function (m, a, z, e) {
       var s, t;
@@ -11,18 +13,20 @@ function App() {
       if (!t) {
         t = new Date().getTime();
         try {
-          m.sessionStorage.setItem('maze-us', t as any);
+          m.sessionStorage.setItem('maze-us', t);
         } catch (err) {}
       }
       s = a.createElement('script');
       s.src = z + '?apiKey=' + e;
       s.async = true;
+      s.onload = () => setIsMazeLoaded(true); // Atualiza o estado quando o script é carregado
       a.getElementsByTagName('head')[0].appendChild(s);
       m.mazeUniversalSnippetApiKey = e;
     })(window, document, 'https://snippet.maze.co/maze-universal-loader.js', '4251a303-735e-4187-b751-5550fe15943c');
   }, []);
+
   const handleMazeButtonClick = () => {
-    if (window.mazeUniversalSnippetApiKey) {
+    if (isMazeLoaded) {
       if (window.Maze) {
         window.Maze.activatePrompt();
       } else {
@@ -32,6 +36,7 @@ function App() {
       console.error('Maze API key not found. Ensure the Maze snippet is correctly integrated.');
     }
   };
+
   return (
     <div className="App">
       <button onClick={handleMazeButtonClick}>Ativar Maze</button>
