81fe29a55ae6f2b74131ceb14a26d57fe0ecfc31
mmartinsoliv <mathews_2010@outlook.com>
2024-07-25 08:42:06 -0300

    add script

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,40 @@
-import './App.css'
+import React, { useState } from 'react';
 
-function App() {
-    return (
-      <div className="App">
-        <button>Ativar Maze de novo</button>
-      </div>
-    );
-  }
+const App = () => {
+  const [scriptLoaded, setScriptLoaded] = useState(false);
 
-export default App
+  const loadScript = () => {
+    if (scriptLoaded) return;
+
+    (function (m, a, z, e) {
+      var s, t;
+      try {
+        t = m.sessionStorage.getItem('maze-us');
+      } catch (err) {}
+
+      if (!t) {
+        t = new Date().getTime();
+        try {
+          m.sessionStorage.setItem('maze-us', t);
+        } catch (err) {}
+      }
+
+      s = a.createElement('script');
+      s.src = z + '?apiKey=' + e;
+      s.async = true;
+      a.getElementsByTagName('head')[0].appendChild(s);
+      m.mazeUniversalSnippetApiKey = e;
+    })(window, document, 'https://snippet.maze.co/maze-universal-loader.js', '4251a303-735e-4187-b751-5550fe15943c');
+
+    setScriptLoaded(true);
+  };
+
+  return (
+    <div>
+      <button onClick={loadScript}>Load Maze Iframe</button>
+    </div>
+  );
+};
+
+export default App;
+
